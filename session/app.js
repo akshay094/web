9020ee0ed1a816f8a1fc69616c65dcc0f2e3a735
Akshay kanojia <akanojia.47@gmail.com>
2022-01-31 19:07:01 +0530

    Create : Session to uniquely identify client

---
+++ session/app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    originalMaxAge: 3600 * 24,
+    _expires: 3600
+  }
+}));
+
+app.get('/', (req, res) => {
+  if (!req.session.visits) req.session.visits = 0;
+  req.session.visits++;
+  res.send(`${req.session.visits}`);
+})
+
+app.listen(PORT, function () {
+  console.log('Listening on port : ', PORT)
+});
